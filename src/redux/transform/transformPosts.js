af835ebb07c800a5a96a37e4379f3c5a4d28c172
Harry <harry811016@gmail.com>
2022-05-09 12:06:29 +0800

    feat: format date

--- src/redux/transform/transformPosts.js
+++ src/redux/transform/transformPosts.js
@@ -1,3 +1,12 @@
+import dayjs from "dayjs";
+import timezone from "dayjs/plugin/timezone";
+import advancedFormat from "dayjs/plugin/advancedFormat";
+import relativeTime from "dayjs/plugin/relativeTime";
+
+dayjs.extend(timezone);
+dayjs.extend(advancedFormat);
+dayjs.extend(relativeTime);
+
 export default function transformPosts({ posts }) {
   return posts.map(
     ({ id, url, text, post_at: postAt, like_count: likeCount, reply_count: replyCount }) => {
@@ -5,7 +14,7 @@ export default function transformPosts({ posts }) {
         id,
         url,
         text,
-        postAt,
+        postAt: dayjs(postAt).fromNow(),
         likeCount,
         replyCount,
       };
